016503372c6538f0079041a3cccae0e06a8ce0e2
Magnus Walbeck <mw@mwalbeck.org>
2022-02-25 14:58:06 +0100

    Initial commit

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Adminly Dashboard
+ *
+ * @copyright Copyright (C) 2022  Magnus Walbeck <magnus@metaprovide.org>
+ *
+ * @author Magnus Walbeck <magnus@metaprovide.org>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Adminly_Dashboard\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+class Application extends App implements IBootstrap
+{
+
+    /** @var string */
+    public const APP_NAME = 'adminly_dashboard';
+
+    /** @var string */
+    protected $appName;
+
+    public function __construct()
+    {
+        parent::__construct(self::APP_NAME);
+        $this->appName  = self::APP_NAME;
+    }
+
+    public function register(IRegistrationContext $context): void
+    {
+    }
+
+    public function boot(IBootContext $context): void
+    {
+    }
+}
---
+++ lib/Controller/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Adminly Dashboard
+ *
+ * @copyright Copyright (C) 2022  Magnus Walbeck <magnus@metaprovide.org>
+ *
+ * @author Magnus Walbeck <magnus@metaprovide.org>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Adminly_Dashboard\Controller;
+
+use OCP\IRequest;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Controller;
+
+class DashboardController extends Controller {
+
+	public function __construct(string $AppName, IRequest $request){
+		parent::__construct($AppName, $request);
+	}
+
+	/**
+	 * @NoAdminRequired
+	* @NoCSRFRequired
+	*/
+	public function index() {
+		return new TemplateResponse('adminly_dashboard', 'dashboard');
+	}
+
+}
